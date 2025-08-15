9ee8202011272cfd5baf6952ac292edc67e9a8d9
Lucien Lu <lucien.7789@gmail.com>
2025-08-16 04:57:43 +1000

    Cool bubbly background for website

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,23 @@ import { useState } from "react";
 import Game from "./components/game";
 
 function App() {
-  const [tab, setTab] = useState<"gallery" | "game">("gallery")
+  const [tab, setTab] = useState<"gallery" | "game">("gallery");
   return (
     <>
       <header className="fixed top-0 right-0 px-16 py-4">
-        <button onClick={() => setTab("gallery")}>
-          Gallery
-        </button>
-        <button onClick={() => setTab("game")}>
-          Game
-        </button>
+        <button onClick={() => setTab("gallery")}>Gallery</button>
+        <button onClick={() => setTab("game")}>Game</button>
       </header>
-      {
-        tab === "gallery" && <div>
-          
-        </div>
-      }
-      {
-        tab === "game" && <div className="h-screen overflow-hidden">
-        <Game />
+      <div className="h-screen overflow-hidden ">
+        <div className="fixed inset-0 bg-[linear-gradient(319deg,#6a5acd_0%,#c54b8c_37%,#b284be_100%)] opacity-40" />
+        <div className="fixed rounded-full h-96 w-96 top-[20vh] left-[20vw] bg-[rgba(106,90,205,0.3)] opacity-40 shadow-[rgba(106,90,205,0.3)] shadow-lg animate-better-pulse-1" />
+        <div className="fixed rounded-full h-64 w-64 top-[40vh] left-[25vw] bg-[rgba(106,90,205,0.4)] opacity-40 shadow-[rgba(106,90,205,0.4)] shadow-lg animate-better-pulse" />
+        <div className="fixed rounded-full h-56 w-56 top-[80vh] left-[80vw] bg-[rgba(106,90,205,0.4)] opacity-40 shadow-[rgba(106,90,205,0.4)] shadow-lg animate-better-pulse-2" />
+        <div className="fixed rounded-full h-84 w-84 top-[30vh] left-[90vw] bg-[rgba(106,90,205,0.4)] opacity-40 shadow-[rgba(106,90,205,0.4)] shadow-lg animate-better-pulse-3" />
+
+        {tab === "gallery" && <div></div>}
+        {tab === "game" && <Game />}
       </div>
-      }
     </>
   );
 }
